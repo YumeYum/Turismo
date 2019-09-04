c74a96e7f33071522cf916c3a866fe9ea5f80f14
YumeYum <ig.zamudio@alumnos.duoc.cl>
2019-09-03 22:01:29 -0400

    Add project files.

---
+++ webapi_turismo/Models/ApplicationSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi_turismo.Models
+{
+    public class ApplicationSettings
+    {
+        public string JWT_Secret { get; set; }
+        public string Cliente_URL { get; set; }
+    }
+}
---
+++ webapi_turismo/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi_turismo.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        [Column(TypeName="nvarchar(150)")]
+        public string FullName { get; set; }
+        [Column(TypeName = "nvarchar(150)")]
+        public string Rut { get; set; }
+    }
+}
